f2b2399d2e3a7bdab7bd6e8c0c5e9089ec7f3c16
OmarCodes2 <omarcodes2@gmail.com>
2022-05-22 09:10:42 -0400

    Finished Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    
+    let len = word.length - 1;
+    let finalWord = "";
+    
+    for (let i = len; i >= 0; i--) {
+        console.log(i);
+        console.log(word[i]);
+        
+        finalWord = finalWord + word[i];
+        console.log(finalWord);
+        
+    }
+    return finalWord;
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
