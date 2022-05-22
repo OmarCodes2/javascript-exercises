f2c9d69ff57e16850d5df140194b58ab41fc6c00
OmarCodes2 <omarcodes2@gmail.com>
2022-05-22 14:46:27 -0400

    Wrote part of the code but not done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp-32) * (5/9);
+  if (temp + 1.0 != temp.toFixed(1) + 1.0){
+    temp = temp.toFixed(1);
+  }
+  console.log(temp+1.0);
+  console.log(temp.toFixed(1));
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (temp*(9/5))+32;
+  if (temp + 1.0 != temp.toFixed(1) + 1.0){
+    temp = temp.toFixed(1);
+  }
+  console.log(temp+1.0);
+  return temp;
 };
 
+console.log(ftoc(32))
 // Do not edit below this line
 module.exports = {
   ftoc,
