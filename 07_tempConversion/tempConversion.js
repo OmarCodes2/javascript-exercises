af9afb032ba2029e268361d7f2577e30b8e5d587
OmarCodes2 <omarcodes2@gmail.com>
2022-05-22 17:30:35 -0400

    Finished Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,23 +1,16 @@
 const ftoc = function(temp) {
   temp = (temp-32) * (5/9);
-  if (temp + 1.0 != temp.toFixed(1) + 1.0){
-    temp = temp.toFixed(1);
-  }
-  console.log(temp+1.0);
-  console.log(temp.toFixed(1));
-  return temp;
+  let n = temp.toFixed(1);
+  return parseFloat(n);
 };
 
 const ctof = function(temp) {
   temp = (temp*(9/5))+32;
-  if (temp + 1.0 != temp.toFixed(1) + 1.0){
-    temp = temp.toFixed(1);
-  }
-  console.log(temp+1.0);
-  return temp;
+  let n = temp.toFixed(1);
+  return parseFloat(n);
 };
 
-console.log(ftoc(32))
+
 // Do not edit below this line
 module.exports = {
   ftoc,
