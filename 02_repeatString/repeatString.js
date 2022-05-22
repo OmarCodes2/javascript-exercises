fa3ff7158b3da891b520d73c98bb4f44f0a04cd5
OmarCodes2 <omarcodes2@gmail.com>
2022-05-22 08:36:32 -0400

    Finished exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(message, iterations) {
+    if (iterations <0){return 'ERROR';}
+    let word = "";
+    for (let i = 0; i < iterations; i++){
+        word = word + message;
+    }
+    return word;
 
 };
 
