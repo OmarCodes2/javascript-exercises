7156215cc9cb0652d2b2027647696ecf0f38b07f
OmarCodes2 <omarcodes2@gmail.com>
2022-05-22 14:25:29 -0400

    Finished Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    if(num%4 == 0 ){
 
+        if(num%100 == 0 && num%400 == 0){
+            return true;
+        }else if(num%100 != 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }else{
+        console.log(num%400);
+        return false;
+    }
 };
 
 // Do not edit below this line
