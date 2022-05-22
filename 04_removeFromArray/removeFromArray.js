8684208a12607c3e2d217306a6a45589b29469ec
OmarCodes2 <omarcodes2@gmail.com>
2022-05-22 10:34:09 -0400

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+    let args = [].slice.call(arguments);
+    args.shift();
+    len = args.length;
+    
+    if (len == 0){
+        return list;
+    }
+    
+    for (let i = 0; i < len; i++){
+        console.log(args[i]);
+
+        pos = list.indexOf(args[i]);
+        if (pos != -1){
+           list.splice(pos,1); 
+        }    
+        
+
+    }
+    return list;
 
 };
-
+console.log(removeFromArray([1,2,3,4]));
 // Do not edit below this line
 module.exports = removeFromArray;
