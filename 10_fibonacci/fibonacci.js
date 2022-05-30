59773d725d01e0c99a2d8e8c03615b1a911c95aa
OmarCodes2 <omarcodes2@gmail.com>
2022-05-29 21:14:13 -0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if (number < 0){return "OOPS"}
+    if (number == 1){return 1};
+    current = 1;
+    total = 1;
+    lastNum = 0;
+    for (let i = 2; i <= number; i++){
+        current = total;
+        total = total + lastNum;
+        lastNum = current;
 
+    }
+    return total;
 };
-
+// console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
