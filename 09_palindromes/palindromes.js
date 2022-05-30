683e85fa56746e1360cd3f79d2aa47b5c9e85364
OmarCodes2 <omarcodes2@gmail.com>
2022-05-29 20:51:59 -0400

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    const punctuation = [",", ".", "!"];
+    let list = word.split(/[!., ]+/);
+    let length = list.length-1;
+    const total= list.reduce((total, word) => {
+        return total + word;
+      }, "");
+    console.log(total);
+    length = total.length;
+    let newWord = "";
+    for( let i = length-1; i >= 0;i--){
+        newWord = newWord + total[i];
+    };
+    if (newWord == total){
+        return true;
+    }
+    return false;
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
