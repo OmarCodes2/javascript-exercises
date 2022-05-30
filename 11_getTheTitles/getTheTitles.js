decc09e2fc8db7fb3d6c8ee83ae658577c286c0c
OmarCodes2 <omarcodes2@gmail.com>
2022-05-30 10:33:03 -0400

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    console.log(list);
+    const theTitle = list.map(book => book.title);
+    return theTitle;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
